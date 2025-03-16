11ff072a18408ba926bcfedd75aec92ab9ca0ab3
vinimax <vini.ciusang@gmail.com>
2025-03-16 18:21:39 -0300

    Created template of initial pages and styles

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Listagem de Jogos</title>
+</head>
+<body>
+    <div id="corpo">
+        <h1>Escolha seu jogo</h1>
+        <table class="listagem">
+            <tr>
+                <td>Foto</td>
+                <td>Nome</td>
+                <td>Adm</td>
+            </tr>
+            <tr>
+                <td>Foto</td>
+                <td>Nome</td>
+                <td>Adm</td>
+            </tr>
+            <tr>
+                <td>Foto</td>
+                <td>Nome</td>
+                <td>Adm</td>
+            </tr>
+            <tr>
+                <td>Foto</td>
+                <td>Nome</td>
+                <td>Adm</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
